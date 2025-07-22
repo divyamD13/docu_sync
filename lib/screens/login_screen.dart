c6d0d7a6876d3c288404e033d606661c72ab2d9f
Divyam Divesh <divyamdivesh13@gmail.com>
2025-07-22 20:37:20 +0530

    login screen ui done

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -101,7 +101,6 @@ class _LoginScreenState extends State<LoginScreen> {
                         ),
                       ),
                     ),
-                  
                 ],
               ),
             ),
