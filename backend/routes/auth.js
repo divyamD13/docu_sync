2d13264b74d07217d5adff57972086b674c8b1d7
Divyam Divesh <divyamdivesh13@gmail.com>
2025-08-02 21:44:54 +0530

    setted up backend server

---
+++ backend/routes/auth.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const authRouter = express();
+
+authRouter.post("/api/signup", async(req,res)=>{
+    try{
+        const {name, email, profilePic} = req.body;
+
+        let user = await User.findOne({email: email});
+
+        if(!user){
+            user = new User({
+                name,
+                email,
+                profilePic
+            });
+
+            await user.save();
+        }
+        res.json({user});
+    }catch(e){
+
+    }
+})
