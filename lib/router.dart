d4ec1a456e179439bca7803311ff109d67e41196
Divyam Divesh <divyamdivesh13@gmail.com>
2025-10-07 21:41:04 +0530

    changed app icon and name

--- lib/router.dart
+++ lib/router.dart
@@ -1,4 +1,3 @@
-
 import 'package:docu_sync/screens/home_screen.dart';
 import 'package:docu_sync/screens/login_screen.dart';
 import 'package:docu_sync/screens/splash_screen.dart';
@@ -7,11 +6,14 @@ import 'package:flutter/material.dart';
 import 'package:routemaster/routemaster.dart';
 
 final loggedOutRoute = RouteMap(routes: {
-  '/': (route) => const MaterialPage(child: LoginScreen()),
+  '/': (route) => const MaterialPage(child: SplashScreen()),
+  '/login': (route) => const MaterialPage(child: LoginScreen()),
+  '/home': (route) => const MaterialPage(child: HomeScreen()),
 });
 
 final loggedInRoute = RouteMap(routes: {
-  '/': (route) => const MaterialPage(child: HomeScreen()),
+  '/': (route) => const MaterialPage(child: SplashScreen()),
+  '/home': (route) => const MaterialPage(child: HomeScreen()),
   '/document/:id': (route) => MaterialPage(
         child: DocumentScreen(
           id: route.pathParameters['id'] ?? '',
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,25 +1,29 @@
 import 'dart:async';
-import 'package:docu_sync/screens/login_screen.dart';
+import 'package:docu_sync/models/user_model.dart';
+import 'package:docu_sync/repository/auth_repository.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:routemaster/routemaster.dart';
 
-class SplashScreen extends StatefulWidget {
+class SplashScreen extends ConsumerStatefulWidget {
   const SplashScreen({super.key});
 
   @override
-  State<SplashScreen> createState() => _SplashScreenState();
+  ConsumerState<SplashScreen> createState() => _SplashScreenState();
 }
 
-class _SplashScreenState extends State<SplashScreen> {
+class _SplashScreenState extends ConsumerState<SplashScreen> {
   @override
   void initState() {
     super.initState();
-
-    Timer(Duration(seconds: 3), () {
-      Navigator.pushReplacement(
-        context,
-        MaterialPageRoute(builder: (context) => LoginScreen()),
-      );
+    Timer(const Duration(seconds: 3), () {
+      final user = ref.read(userProvider);
+      if (user != null && user.token.isNotEmpty) {
+        Routemaster.of(context).replace('/home');
+      } else {
+        Routemaster.of(context).replace('/login');
+      }
     });
   }
 
@@ -31,8 +35,8 @@ class _SplashScreenState extends State<SplashScreen> {
           colors: [
             Color.fromRGBO(0, 121, 107, 1),
             Color.fromRGBO(73, 169, 158, 1),
-            Color.fromRGBO(255, 255, 255, 1)
-          ], 
+            Color.fromRGBO(255, 255, 255, 1),
+          ],
           begin: Alignment.topCenter,
           end: Alignment.bottomCenter,
         ),
